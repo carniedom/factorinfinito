92f25a0f32fee824e6b4f33231fda2eb24e3bc68
carniedom <carniedom@alu.edu.gva.es>
2023-02-08 15:09:07 +0100

    Pregunta Test 2

--- src/net/severo/entdes/Factorial.java
+++ src/net/severo/entdes/Factorial.java
@@ -6,15 +6,18 @@ package net.severo.entdes;
  */
 public class Factorial {
 
+	public static final String VALUE_OF_N = "Value of n: ";
+	public static final String VALUE_OF_N1 = "Value of n!: ";
+
 	public static void main(String[] args) {
 
 		int n = (int) (Math.random() * 10);
 		int result = 1;
-		System.out.println("Value of n: " + n);
+		System.out.println(VALUE_OF_N + n);
 		while (n > 1) {
 			result = result * (n - 1);
 		}
-		System.out.println("Value of n!: " + result);
+		System.out.println(VALUE_OF_N1 + result);
 
 	}
 
